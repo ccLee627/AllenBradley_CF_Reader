91cc5235be2b551f8596dcc064a62740fddd969c
Tony <tscarcia@hotmail.com>
2019-12-07 11:29:32 -0500

    add 50 more setpoints to array

--- read_cf_file.m
+++ read_cf_file.m
@@ -1,4 +1,4 @@
-% Matlab file to read setpoints data file from CF card.
+% Matlab function to read setpoints data file from Allen-Bradley CF/SD card.
 % Written by Tony Scarcia
 % Jan 7, 2018
 function[sp] = read_cf_file(filename)
@@ -7,9 +7,9 @@ fileID = fopen(filename, 'r');
 header =  fread(fileID,[3],'single');
 sp_array1 = fread(fileID,[109],'single');
 spacer = fread(fileID,[2],'single');
-sp_array2 = fread(fileID,[41],'single');
+sp_array2 = fread(fileID,[91],'single');
 fclose(fileID);
-sp_index = [1:150]';
+sp_index = [1:200]';
 sp_array = [sp_array1;sp_array2];
-sp = [sp_index,sp_array]
+sp = [sp_index, sp_array];
 end
